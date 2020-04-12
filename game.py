b4fcecc3b0e11d7f5482786f8d5fbd1aef792d0f
VoxLight <tkkt392@gmail.com>
2020-04-12 15:23:00 -0400

    Added options (wip)
    Added a wip options. Tested to see if the menus work the way I wanted. They do. Options placeholder for later.

--- game.py
+++ game.py
@@ -8,23 +8,31 @@ from pygame.locals import *
 from menu import Menu
 
 
+
+
 def main():
     
     # pygame setup
-    CLOCK = pygame.time.Clock()
-
     pygame.display.set_caption("Breakout")
-
+    CLOCK = pygame.time.Clock()
     SCREEN = pygame.display.set_mode(RES, pygame.RESIZABLE)
-    
+
     # Setup menus for the game
+    op = Menu('Options', SCREEN, CLOCK)
+
+    op.add_item('Resolution')
+    op.add_item('Back', callback='exit')
+
+
     mm = Menu('Main Menu', SCREEN, CLOCK)
-    # TODO: Add Menu.activate as callback for these items
+
     mm.add_item('New Game')
     mm.add_item('Level Select')
     mm.add_item('Stage Editor')
-    mm.add_item('Options')
+    mm.add_item('Options', callback=op.activate)
     mm.add_item('Quit', callback='exit')
+    
+    
 
     # main game loop
     mm.activate()
